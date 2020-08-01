bc12adcbb83c0cdd4f957767470d7f57eeeaf4a1
zhulu <shaonianzhentan@sina.cn>
2020-08-01 23:27:44 +0800

    显示日志

--- node/lib/api.js
+++ node/lib/api.js
@@ -3,14 +3,16 @@ const fetch = require('node-fetch')
 module.exports = class {
     constructor(config) {
         this.config = config
-        this.http = (params) => {
-            return fetch(config.url, {
+        this.http = async (params) => {
+            let res = await fetch(config.url, {
                 method: 'POST',
                 body: JSON.stringify({
                     key: config.key,
                     ...params
                 })
-            })
+            }).then(res => res.json())
+            this.log(res)
+            return res
         }
     }
 
