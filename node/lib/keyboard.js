52f6d432c100df5b8d8e42c69d3dec6012d365af
zhulu <shaonianzhentan@sina.cn>
2020-08-17 09:58:44 +0800

    多个设置监听

--- node/lib/keyboard.js
+++ node/lib/keyboard.js
@@ -2,17 +2,26 @@ const fs = require('fs')
 const InputEvent = require('input-event');
 
 module.exports = function (api) {
-    let dev_input = api.config.keyboard
-    if(!dev_input) return;
+    let kb = api.config.keyboard
+    if (!kb) return;
 
-    api.log('开始监听：', dev_input)
-    if (!fs.existsSync(dev_input)) return;
-    const input = new InputEvent(dev_input);
-    const keyboard = new InputEvent.Keyboard(input);
-    //keyboard.on('keyup'   , console.log);
-    //keyboard.on('keydown' , console.log);
-    keyboard.on('keypress', (data) => {
-        api.log(data)
-        api.fire('keyboard_remote_command_received', { key_code: data.code });
-    });
+    let dev_arr = []
+    if (typeof kb === 'string') {
+        dev_arr.push(kb)
+    } else if (Array.isArray(kb)) {
+        dev_arr = kb
+    }
+
+    dev_arr.forEach((dev_input, index) => {
+        api.log('开始监听：', dev_input)
+        if (!fs.existsSync(dev_input)) return;
+        const input = new InputEvent(dev_input);
+        const keyboard = new InputEvent.Keyboard(input);
+        //keyboard.on('keyup'   , console.log);
+        //keyboard.on('keydown' , console.log);
+        keyboard.on('keypress', (data) => {
+            api.log(data)
+            api.fire('keyboard_remote_command_received', { index, key_code: data.code });
+        });
+    })
 }
