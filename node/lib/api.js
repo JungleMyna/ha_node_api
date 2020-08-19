6920911ad4f6dec7c7caf068403602a11b54fcb9
zhulu <shaonianzhentan@sina.cn>
2020-08-19 10:08:56 +0800

    更新规则

--- node/lib/api.js
+++ node/lib/api.js
@@ -1,21 +1,9 @@
 const fs = require('fs')
-const fetch = require('node-fetch')
 const YAML = require('yaml')
+const HomeAssistant = require('homeassistant')
 
 module.exports = class {
     constructor() {
-        this.http = async (params) => {
-            let config = this.config
-            let res = await fetch(config.url, {
-                method: 'POST',
-                body: JSON.stringify({
-                    key: config.key,
-                    ...params
-                })
-            }).then(res => res.json())
-            this.log(res)
-            return res
-        }
         this.read()
     }
 
@@ -23,6 +11,17 @@ module.exports = class {
         // 读取配置
         const file = fs.readFileSync('./config.yaml', 'utf8')
         this.config = YAML.parse(file)
+        this.connect()
+    }
+
+    connect() {
+        if (this.hass) return;
+        let { url, token } = this.config
+        let link = new URL(url)
+        let host = `${link.protocol}//${link.hostname}`
+        let port = link.port
+        this.hass = new HomeAssistant({ host, port, token, ignoreCert: false });
+        this.log(`连接成功【${url}】`)
     }
 
     log() {
@@ -30,10 +29,17 @@ module.exports = class {
     }
 
     set_state(entity_id, state) {
-        return this.http({ type: 'set_state', entity_id, state })
+        let { hass } = this
+        let arr = entity_id.split('.')
+        hass.states.get(arr[0], arr[1]).then(res => {
+            res['state'] = state
+            hass.states.update(arr[0], arr[1], res);
+        })
+        this.log(`【${entity_id}】更新状态【${state}】`)
     }
 
     fire(event_type, event_data) {
-        return this.http({ type: 'fire_event', event_type, event_data })
+        this.hass.events.fire(event_type, event_data);
+        this.log(`事件名【${event_type}】事件参数【${event_data}】`)
     }
 }
